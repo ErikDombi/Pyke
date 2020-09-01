0d88b0840f9f49cf9ce3ee0f6cdb65391408817e
wickedlizerd <erikd02@gmail.com>
2020-09-01 09:48:05 -0400

    please build aha

--- Pyke.Example/Program.cs
+++ Pyke.Example/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Pyke;
+using Pyke.ChampSelect.Models;
 using Pyke.Events;
 using Pyke.Events.Models;
 using Pyke.Websocket;
@@ -17,16 +18,14 @@ namespace Pyke.Example
         {
             API = new LeagueAPI().ConnectAsync().GetAwaiter().GetResult();
 
-            API.Events.SubscribeEvent(EventType.OnSessionUpdated);
-            API.Events.SubscribeEvent(EventType.GameflowStateChanged);
+            API.Events.SubscribeAllEvents();
             API.Events.GameflowStateChanged += Events_GameflowStateChanged;
-            API.Events.OnSessionUpdated += Events_OnSessionUpdated;
-            API.Events.MatchFoundStatusChanged += Events_MatchFoundStatusChanged;
+            API.Events.OnMatchFound += Events_MatchFoundStatusChanged;
             API.Events.OnChampSelectTurnToPick += Events_OnChampSelectTurn;
             Console.ReadLine();
         }
 
-        private static void Events_OnChampSelectTurn(object sender, ChampSelect.Models.Session e)
+        private static void Events_OnChampSelectTurn(object sender, PickType e)
         {
             Console.WriteLine("it is your turn to select a champion");
         }
@@ -34,11 +33,6 @@ namespace Pyke.Example
         private static void Events_MatchFoundStatusChanged(object sender, ReadyState e)
         {
             API.MatchMaker.AcceptMatch();
-        }
-
-        private static void Events_OnSessionUpdated(object sender, ChampSelect.Models.Session e)
-        {
-
         }
 
         private static void Events_GameflowStateChanged(object sender, State e)
